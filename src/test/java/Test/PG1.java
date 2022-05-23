26ecf8af72c40d9df36ce38467d85db98450e582
mdabrowski <matthaeus.dabrowski@qytera.de>
2022-05-23 10:34:48 +0200

    test v3.0

--- src/test/java/Test/PG1.java
+++ src/test/java/Test/PG1.java
@@ -1,47 +1,48 @@
 package Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.junit.*;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
 
-
-public class PG1 {
-
-    @Test
-    public void testWebDriverBackedSelenium(){
-    // declaration and instantiation of objects/variables
-        System.setProperty("webdriver.chrome.driver","C:\\Webdriver\\chromedriver.exe");
-    WebDriver driver = new ChromeDriver();
-    //comment the above 2 lines and uncomment below 2 lines to use Chrome
-    //System.setProperty("webdriver.chrome.driver","G:\\chromedriver.exe");
-    //WebDriver driver = new ChromeDriver();
-
-    String baseUrl = "http://demo.guru99.com/test/newtours/";
-    String expectedTitle = "Welcome: Mercury Tours";
-    String actualTitle = "";
-
-    // launch Fire fox and direct it to the Base URL
-        driver.get(baseUrl);
-
-    // get the actual value of the title
-    actualTitle =driver.getTitle();
-
-    /*
-     * compare the actual title of the page with the expected one and print
-     * the result as "Passed" or "Failed"
-     */
-        if(actualTitle.contentEquals(expectedTitle))
-
+public class PG1
+{
+    private WebDriver driver;
+    @BeforeClass
+    public void setUp()
     {
-        System.out.println("test.Test Passed!");
-    } else
-
-    {
-        System.out.println("test.Test Failed");
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.navigate().to("https://www.google.com");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(120, TimeUnit.MILLISECONDS);
     }
-
-    //close Fire fox
-        driver.close();
-
-}
+    @Test
+    public void userLogin()
+    {
+        WebElement searchTxt = driver.findElement(By.name("q"));
+        searchTxt.sendKeys("automation");
+        WebElement submitBtn = driver.findElement(By.name("btnK"));
+        submitBtn.click();
+        System.out.println("Current URL is:" + driver.getCurrentUrl());
+        Assert.assertTrue(driver.getTitle().contains("automation - Google Search"));
+        System.out.println("Current Title is:" + driver.getTitle());
+    }
+    @AfterClass
+    public void tearDown(){
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
