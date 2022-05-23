44c4e24539f8fefdc0241bb965c8adfc50428e6d
MatQytera <101273055+MatQytera@users.noreply.github.com>
2022-05-23 09:59:40 +0200

    Update PG1.java

--- src/test/java/Test/PG1.java
+++ src/test/java/Test/PG1.java
@@ -1,30 +1,31 @@
 package Test;
-import io.github.bonigarcia.wdm.WebDriverManager;
+ package SeleniumProject.Maven;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.junit.*;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-
-public class PG1 {
-
-  public WebDriver driver; 
-  public String baseUrl = "https://www.lambdatest.com/";
-    
-    @Test
-    public void testWebDriverBackedSelenium(){
-        
-    WebDriverManager.chromedriver().setup();
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("--no-sandbox");
-    options.addArguments("--disable-dev-shm-usage");
-    options.addArguments("--headless");
-    driver = new ChromeDriver(options);    
-        
-        
-    driver.manage().window().maximize();  
-    driver.get(baseUrl);
-    driver.close();
-
-
-}
+public class AppTest 
+{ 
+	public WebDriver driver; 
+	public String baseUrl = "https://www.lambdatest.com/";  
+	
+	@Test             
+	public void test1() {      
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--headless");
+		driver = new ChromeDriver(options);
+ 
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);  
+		driver.manage().window().maximize();  
+		driver.get(baseUrl);
+		driver.close();
+	}     
 }
